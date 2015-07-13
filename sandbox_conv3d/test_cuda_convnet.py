faa9a1a71961b47503e85d97f68274a44bfe50e7
Pangyu Teng <pteng@medqia.com>
2015-07-13 08:41:45 -0700

    added script to for execution of test case for cuda_convnet.

---
+++ sandbox_conv3d/test_cuda_convnet.py
@@ -0,0 +1,6 @@
+from pylearn2.sandbox.cuda_convnet.tests import test_pool 
+test_pool.test_pool()
+#from pylearn2.sandbox.cuda_convnet.pool import MaxPool
+#ds = 1
+#stride = 1
+#op = MaxPool(ds=ds, stride=stride)
